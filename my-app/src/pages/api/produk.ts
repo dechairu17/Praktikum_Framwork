0cf20a93e951936ccc2ae5d20b656cb02b18d1e7
dechairu17 <dechairu@gmail.com>
2026-04-08 01:05:53 +0700

    JS 7

---
+++ my-app/src/pages/api/produk.ts
@@ -0,0 +1,49 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { retrieveProducts } from "../utils/db/servicefirebase";
+
+type Data = {
+    status: boolean;
+    status_code : number;
+    data: any;
+};
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse<Data>
+) {
+    const data = await retrieveProducts("products");
+    res.status(200).json({ status: true, status_code: 200, data });
+}
+
+// export default function handler(
+//     req: NextApiRequest,
+//     res: NextApiResponse<Data>
+// ) 
+//     {
+//         const data =
+//         [
+//             {
+//                 id: "1",
+//                 nama: "Kaos polos",
+//                 harga: 100000,
+//                 ukuran: "L",
+//                 warna: "Hitam",
+//             },
+//             {
+//                 id: "2",
+//                 nama: "kaos lengan panjang",
+//                 harga: 300000,
+//                 ukuran: "XL",
+//                 warna: "Merah",
+//             },
+//             {
+//                 id: "3",
+//                 nama: "Sepatu",
+//                 harga: 250000,
+//                 ukuran: "43",
+//                 warna: "Hitam",
+//             },            
+//         ];
+//         res.status(200).json({status: true,status_code: 200,data:data });
+//     }
+            
--- my-app/src/pages/produk/index.tsx
+++ my-app/src/pages/produk/index.tsx
@@ -1,3 +1,47 @@
 import ProdukPage from "../../views/produk";
+import { useEffect, useState } from "react";
 
-export default ProdukPage;
+type ProductType = {
+    id: string;
+    name: string;
+    price: number;
+    size: string;
+    category: string;
+};
+
+const kategori = () => {
+    const [products, setProducts] = useState<ProductType[]>([]);
+
+    const fetchProducts = () => {
+        fetch("/api/produk")
+            .then((response) => response.json())
+            .then((responsedata) => {
+                //console.log("Data produk:", responsedata.data);
+                setProducts(responsedata.data);
+            })
+            .catch((error) => {
+                console.error("Error fetching produk:", error);
+            });
+    };
+
+    useEffect(() => {
+        fetchProducts();
+    }, []);
+
+    return (
+        <div>
+            <h1>Daftar Produk</h1>
+            <button onClick={fetchProducts}>Refresh Data</button>
+            {products.map((product: ProductType) => (
+                <div key={product.id}>
+                    <h2>{product.name}</h2>
+                    <p>Harga: {product.price}</p>
+                    <p>Ukuran: {product.size}</p>
+                    <p>Kategori: {product.category}</p>
+                </div>
+            ))}
+        </div>
+    );
+};
+
+export default kategori;
